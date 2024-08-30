b0a73a9d7f64ce9ed1caa50053800a2d2a09c2fd
Jaswant Singh <jaswantrajpoot060@gmail.com>
2024-08-30 16:53:35 +0530

    Changes fixed

--- CRUD_Entityframework/Controllers/CurrencyController.cs
+++ CRUD_Entityframework/Controllers/CurrencyController.cs
@@ -1,6 +1,7 @@
 ﻿using CRUD_Entityframework.Data;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CRUD_Entityframework.Controllers
 {
@@ -16,11 +17,31 @@ namespace CRUD_Entityframework.Controllers
         }
 
         [HttpGet("")]
-        public IActionResult GetAllCurrencies()
+        public async Task<IActionResult> GetAllCurrencies()
         {
             //var res = _appDbContext.tblCurrency.ToList();
-            var res = (from currenries in _appDbContext.tblCurrency
-                      select currenries).ToList();
+            var res = await (from currenries in _appDbContext.tblCurrency
+                             select currenries).ToListAsync();
+            return Ok(res);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GeByIdCurrencies([FromRoute] int id)
+        {
+            //var res = _appDbContext.tblCurrency.FindAsync(id);
+            var res = await (from c in _appDbContext.tblCurrency
+                             where c.Id == id  // Replace Id with the actual column name of the primary key
+                             select c).FirstOrDefaultAsync();
+            return Ok(res);
+        }
+
+        [HttpGet("{name}")]
+        public async Task<IActionResult> GeByNameCurrencies([FromRoute] string name)
+        {
+           //var res = _appDbContext.tblCurrency.Where(x=>x.Title == name).FirstOrDefaultAsync();    
+            var res = await (from c in _appDbContext.tblCurrency
+                             where c.Title == name  // Replace Id with the actual column name of the primary key
+                             select c).FirstOrDefaultAsync();
             return Ok(res);
         }
 
